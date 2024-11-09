4f8797996b7f7891ee85df72d215f6b564cc8aa0
Mark Chris Levy <levy@codeandmore.pl>
2024-11-09 15:51:32 +0100

    commit

--- src/preferences/public.ts
+++ src/preferences/public.ts
@@ -1,5 +1,5 @@
 export {PreferencesCollectionRefImpl} from "./PreferencesCollectionRefImpl.js";
-export * from "./interfaces.js";
+export {PreferencesCollectionRef, PreferencesItemRef, PreferencesSetOptions, PreferencesItem, PreferencesItemEvent, PreferencesItemEventListener, PreferencesItemEventType, PreferencesItemValueRef, PreferencesContainer, PreferencesFilter} from "./interfaces.js";
 export {PreferencesItemRefImpl} from "./PreferencesItemRefImpl.js";
 export {MemoryPreferencesContainer, MemoryPreferencesContainerItem} from "./MemoryPreferencesContainer.js";
 export {StoragePreferencesContainer} from "./StoragePreferencesContainer.js";
